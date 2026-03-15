0636bb7ddc66545ea627f8885bf736b97ff5434f
Mohammed Boussardi <mohammedboussardi@Mohammeds-MacBook-Pro.local>
2026-03-15 23:17:56 +0000

    Deploy new Next.js portfolio to GitHub Pages

---
+++ _next/static/DSdfSLjzP0mw0o0H2EC0b/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fcertifications","\u002F[locale]\u002Fcontact","\u002F[locale]\u002Fexperience","\u002F[locale]\u002Fskills"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
